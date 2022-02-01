f84ec0c61fd6401dddfca146e2e6389ba8a90d11
kylo74 <kyloc74@gmail.com>
2022-02-01 21:28:58 +0000

    Imported randint, created scores dictionary, and created board class

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+scores = {'computer':0,'player':0}
+
+class Board:
+    """
+    This class sets board size, number of ships,player name,
+    board type (player or computer) and has methods for 
+    adding ships and geusses and printing the board
+    """
+    
