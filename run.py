2e23e66e62eeb966e5a3a7c06ba81420980192e6
kylo74 <kyloc74@gmail.com>
2022-02-01 22:06:23 +0000

    Add methods to board class

--- run.py
+++ run.py
@@ -8,4 +8,18 @@ class Board:
     board type (player or computer) and has methods for 
     adding ships and geusses and printing the board
     """
-    
+    def __init__(self, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type 
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+
+    def guess(self, x, y):
+        self
